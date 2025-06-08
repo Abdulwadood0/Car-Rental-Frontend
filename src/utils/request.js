84b1b51852768641d686538cad7694b91988660d
Abdul Wadoud <abdwadood.kr@gmail.com>
2025-06-08 19:14:31 +0300

    changed request url

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const request = axios.create({
     // baseURL: "http://localhost:5000"
-    baseURL: "http://43.204.107.146"
+    baseURL: "https://43.204.107.146"
 })
 
 export default request
