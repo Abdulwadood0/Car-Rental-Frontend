5b9a447ed7319f563b3cd88e1c835e667bea6644
Abdul Wadoud <abdwadood.kr@gmail.com>
2025-06-11 12:14:52 +0300

    fix routing issues

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const request = axios.create({
-    baseURL: "https://carrent.space"
-    // baseURL: "http://localhost:5000"
+    // baseURL: "https://carrent.space"
+    baseURL: "http://localhost:5000"
 })
 
 export default request
